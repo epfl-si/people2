ba0baa5ad77d202aa44fd82af2806ab6b8cc15b1
D4rkHeart <leonardo.surdez@gmail.com>
2025-01-23 17:02:12 +0100

    [feat] update logic for cropped images and enhance create action
    - Implemented `update` action to handle cropped image uploads.
      - Added logic to process and attach cropped images from a data URL.
    - Enhanced `create` action to support Turbo Stream responses.
      - Ensures proper association of pictures with profiles during creation.
    - Introduced helper method `data_url_to_uploaded_file` to decode and handle base64-encoded image data.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -19,10 +19,6 @@ class PicturesController < BackendController
   # for an alternative solution, have a look to
   # https://medium.com/@fabriciobonjorno/upload-profile-image-in-real-time-1c74313a1116
   # POST /profile/profile_id/pictures or /profile/profile_id/pictures.json
-  def create
-    if @profile.pictures.count + 1 > Rails.application.config_for(:limits).max_profile_pictures
-      raise "Max number of profile pictures reached"
-    end
 
   def crop
     respond_to do |format|
@@ -36,26 +32,28 @@ class PicturesController < BackendController
     end
   end
 
-  # # PATCH/PUT /pictures/1 or /pictures/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @picture.update(picture_params)
-  #       format.turbo_stream do
-  #         flash.now[:success] = "flash.generic.success.update"
-  #         render :update
-  #       end
-  #       format.json { render :show, status: :ok, location: @picture }
-  #     else
-  #       format.turbo_stream do
-  #         flash.now[:error] = "flash.generic.error.update"
-  #         render :edit, status: :unprocessable_entity, locals: { profile: @profile, picture: @picture }
-  #       end
-  #       format.json { render json: @picture.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  def update
+    unless params[:picture].present? && params[:picture][:cropped_image].present?
+      render json: { error: "No cropped image data provided" }, status: :unprocessable_entity and return
+    end
 
-  # DELETE /pictures/1 or /pictures/1.json
+    begin
+      file = data_url_to_uploaded_file(params[:picture][:cropped_image])
+      @picture.cropped_image.attach(file)
+    rescue StandardError
+      flash.now[:error] = "flash.generic.error.update"
+      render json: { error: "Unable to process cropped image" }, status: :unprocessable_entity and return
+    end
+
+    respond_to do |format|
+      format.turbo_stream do
+        flash.now[:success] = "flash.generic.success.update"
+        render :update
+      end
+      format.html { redirect_to picture_path(@picture), notice: "Picture updated successfully." }
+    end
+  end
+
   def destroy
     raise "Cannot delete selected photo" if @picture.selected?
 
@@ -76,25 +74,61 @@ class PicturesController < BackendController
     end
   end
 
+  def create
+    @profile = Profile.find(params[:profile_id])
+
+    if @profile.pictures.count + 1 > Rails.application.config_for(:limits).max_profile_pictures
+      raise "Max number of profile pictures reached"
+    end
+
+    @picture = @profile.pictures.new(picture_params)
+    respond_to do |format|
+      if @picture.save
+        format.turbo_stream do
+          flash.now[:success] = "flash.generic.success.create"
+          render :create, locals: { profile: @profile, picture: @picture }
+        end
+        format.json { render :show, status: :created, location: @picture }
+      else
+        format.turbo_stream do
+          flash.now[:error] = "flash.generic.error.create"
+          render :new, status: :unprocessable_entity, locals: { profile: @profile, picture: @picture }
+        end
+        format.json { render json: @picture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
-  # def append_picture
-  #   render turbo_stream: turbo_stream.append("jobs",
-  #                                            partial: "editable_picture",
-  #                                            locals: { picture: @picture })
-  # end
-
   def set_profile
-    @profile = Profile.find(params[:profile_id])
+    @profile = @picture.profile
   end
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_picture
     @picture = Picture.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Picture not found" }, status: :not_found
   end
 
-  # Only allow a list of trusted parameters through.
   def picture_params
-    params.require(:picture).permit(:image)
+    params.require(:picture).permit(:cropped_image, :image)
+  end
+
+  def data_url_to_uploaded_file(data_url)
+    content_type = data_url[/data:(.*?);base64,/, 1]
+    decoded_data = Base64.decode64(data_url.split(",")[1])
+    filename = "cropped_image.#{content_type.split('/').last}"
+
+    tempfile = Tempfile.new(filename)
+    tempfile.binmode
+    tempfile.write(decoded_data)
+    tempfile.rewind
+
+    ActionDispatch::Http::UploadedFile.new(
+      tempfile: tempfile,
+      filename: filename,
+      type: content_type
+    )
   end
 end
