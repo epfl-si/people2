6eaf7f1c6ead2fa49d50e4f4fd7665f23b80c677
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-01-24 17:00:04 +0100

    [fix] missing Achievement in the list of allowed subtypes

--- app/models/index_box.rb
+++ app/models/index_box.rb
@@ -9,7 +9,7 @@
 #  - experience
 
 class IndexBox < Box
-  ALLOWED_SUBKINDS = %w[Award Education Experience Publication Social].freeze
+  ALLOWED_SUBKINDS = %w[Achievement Award Education Experience Publication Social].freeze
 
   translates_rich_text :content
 
--- app/models/legacy/experience.rb
+++ app/models/legacy/experience.rb
@@ -8,5 +8,30 @@ module Legacy
     ollamizes :title
 
     belongs_to :cv, class_name: 'Cv', foreign_key: 'sciper', inverse_of: :experiences
+
+    # DATES_RE = /([0-9]{4}).*?([0-9]{4})?/
+    DATES_RE = /([0-9]{4})(.*?([0-9]{4}))?/
+
+    def year_begin
+      years[0]
+    end
+
+    def year_end
+      years[1]
+    end
+
+    def years
+      @years ||= begin
+        m = DATES_RE.match(dates)
+        if m.present?
+          yb = m[1]
+          ye = m[3] || yb
+        else
+          yb = nil
+          ye = nil
+        end
+        [yb, ye]
+      end
+    end
   end
 end
