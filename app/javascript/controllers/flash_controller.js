3749578cf59b7c60c939ac7cf8b6dfd173966867
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-01-26 16:05:47 +0100

    [fix] missing vendor folder in src sync to test server

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,14 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("flash controller connect");
     setTimeout(() => {
       this.dismiss();
     }, 3000);
   }
 
   dismiss() {
-    console.log("flash controller dismiss");
     this.element.remove();
   }
 }
