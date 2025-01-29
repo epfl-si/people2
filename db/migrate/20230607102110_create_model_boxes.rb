db4ee9644fe2bc099a80bc7d1b0f3d0820ce600b
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-01-29 12:55:50 +0100

    [feat] available boxes can now be added in edit

--- db/migrate/20230607102110_create_model_boxes.rb
+++ db/migrate/20230607102110_create_model_boxes.rb
@@ -5,12 +5,17 @@ class CreateModelBoxes < ActiveRecord::Migration[7.0]
     create_table :model_boxes do |t|
       t.string     :kind, default: 'RichTextBox'
       t.string     :subkind, null: true
+      t.integer :max_copies, default: 1
       t.references :section, null: false, foreign_key: true
       t.string :label, null: false
       t.string :title_en
       t.string :title_fr
       t.string :title_it
       t.string :title_de
+      t.string :description_en
+      t.string :description_fr
+      t.string :description_it
+      t.string :description_de
       t.boolean :standard, default: true
       t.boolean :show_title, default: true
       t.boolean :locked_title, default: true
