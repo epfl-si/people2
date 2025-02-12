7670eb98844f21004ec9343ac53ce90a43316ca9
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-02-12 10:52:43 +0100

    [fix] compatibility with action_policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,4 +52,8 @@ class ApplicationController < ActionController::Base
   def register_client_origin
     @is_intranet_client = Rails.configuration.intranet_re.match?(request.remote_ip)
   end
+
+  def current_user
+    Current.user
+  end
 end
