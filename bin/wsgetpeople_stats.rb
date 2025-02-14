b8d90d0160ced9e3e4f00e9b9494ccc187f37e27
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-02-14 22:52:35 +0100

    [feat] add usage stats about units parameter of wsgetpeople

--- bin/wsgetpeople_stats.rb
+++ bin/wsgetpeople_stats.rb
@@ -22,6 +22,8 @@ uni_count={} # units values
 gru_count={} # groups values
 pco_count={} # progcode values
 stu_count={} # structure + unit parameter
+uni_histo={} # histogram of how many times a given number of units was used
+muu_list={}  # multiple units combinations
 File.readlines(srcfile, chomp: true).each do |line|
 	# log file where already sort|uniq -c so the first column contains the multiplicity
 	c, ip, req = line.split(" ")
@@ -44,6 +46,11 @@ File.readlines(srcfile, chomp: true).each do |line|
 	end
 	unless (str=params["units"]).nil?
 		uni_count[str] = (uni_count[str]||0) + c
+		nu = str.split(",").count
+		uni_histo[nu] = (uni_histo[nu]||0) + c
+		if nu > 3
+			muu_list[str] = (muu_list[str]||0) + c
+		end
 	end
 	unless (str=params["groups"]).nil?
 		gru_count[str] = (gru_count[str]||0) + c
@@ -91,4 +98,15 @@ end
 printf("# ------------- Values for unit / structure parameter\n")
 stu_count.to_a.sort{|a,b| a[1] <=> b[1]}.each do |k,c|
 	printf("%8d %s\n", c, k);
+end
+
+printf("# ------------- Histogram of how many units in units parameter\n")
+printf("  %8s   %8s\n", "units", "times")
+uni_histo.keys.sort.each do |c|
+	printf("  %8d   %8d\n", c, uni_histo[c])
+end
+
+printf("# ------------- Multiple units combinations\n")
+muu_list.to_a.sort{|a,b| a[1] <=> b[1]}.each do |v|
+	printf("%8d %s\n", v[1], v[0])
 end
