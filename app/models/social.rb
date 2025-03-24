aea0572e5eab817318a22ceaad5c94f0748a4ca2
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-03-24 10:55:32 +0100

    [chore] move social icons to EPFL Elements
    this also refactor part of the social links feature

--- app/models/social.rb
+++ app/models/social.rb
@@ -6,114 +6,137 @@
 class Social < ApplicationRecord
   include AudienceLimitable
 
-  RESEARCH_IDS = {
-    'orcid' => {
-      'img' => 'ORCIDiD_icon16x16.png',
-      'url' => 'https://orcid.org/XXX',
-      'placeholder' => '0000-0002-1825-0097',
-      'label' => 'ORCID',
-      'position' => 0,
-      'icon' => "orcidid",
-      're' => /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/,
-      'help' => {
+  RESEARCH_IDS_LIST = [
+    {
+      tag: 'orcid',
+      # img: 'ORCIDiD_icon16x16.png',
+      url: 'https://orcid.org/XXX',
+      placeholder: '0000-0002-1825-0097',
+      label: 'ORCID',
+      position: 0,
+      icon: "icon-orcid",
+      re: /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/,
+      help: {
         'en' => 'https://orcid-integration.epfl.ch/',
         'fr' => 'https://orcid-integration.epfl.ch/'
       }
     },
-    'wos' => {
-      'img' => 'publons.png',
-      'url' => 'https://www.webofscience.com/wos/author/rid/XXX',
-      'placeholder' => 'AAX-5119-2020',
-      'label' => 'Publons - Web of Science ID',
-      'position' => 1,
-      'icon' => "publons",
-      're' => /^([A-Z]+-[0-9]{4}-[0-9]{4}|[0-9])+$/
+    {
+      tag: 'wos',
+      # img: 'publons.png',
+      url: 'https://www.webofscience.com/wos/author/record/XXX',
+      placeholder: 'AAX-5119-2020',
+      label: 'Publons - Web of Science ID',
+      position: 1,
+      icon: "icon-wos",
+      re: /^([A-Z]+-[0-9]{4}-[0-9]{4}|[0-9])+$/
     },
-    'scopus' => {
-      'img' => 'scopus.png',
-      'url' => 'https://www.scopus.com/authid/detail.uri?authorId=XXX',
-      'placeholder' => '57192201516',
-      'label' => 'Scopus ID',
-      'position' => 2,
-      'icon' => "scopus",
-      're' => /^[0-9]+$/
+    {
+      tag: 'scopus',
+      img: 'scopus.png',
+      url: 'https://www.scopus.com/authid/detail.uri?authorId=XXX',
+      placeholder: '57192201516',
+      label: 'Scopus ID',
+      position: 2,
+      # icon: "icon-scopus",
+      re: /^[0-9]+$/
     },
-    'googlescholar' => {
-      'img' => 'google_scholar.png',
-      'url' => 'https://scholar.google.com/citations?user=XXX',
-      'placeholder' => 'abcdEFGhiJKLMno',
-      'label' => 'Google Scholar ID',
-      'position' => 3,
-      'icon' => 'google',
-      're' => /^[0-9a-zA-Z.-]+$/
+    {
+      tag: 'googlescholar',
+      # img: 'google-scholar.svg',
+      url: 'https://scholar.google.com/citations?user=XXX',
+      placeholder: 'abcdEFGhiJKLMno',
+      label: 'Google Scholar ID',
+      position: 3,
+      icon: 'icon-googlescholar',
+      re: /^[0-9a-zA-Z.-]+$/
     },
-    'linkedin' => {
-      'img' => 'linkedin.jpg',
-      'url' => 'https://www.linkedin.com/in/XXX',
-      'placeholder' => 'john-doe-12345',
-      'label' => 'Linkedin ID',
-      'position' => 4,
-      'icon' => 'linkedin',
-      're' => %r{^[a-z][a-z0-9-]+/?$}
+    {
+      tag: 'linkedin',
+      # img: 'linkedin.jpg',
+      url: 'https://www.linkedin.com/in/XXX',
+      placeholder: 'john-doe-12345',
+      label: 'Linkedin',
+      position: 4,
+      icon: 'linkedin',
+      re: %r{^[a-z][a-z0-9-]+/?$}
     },
-    'github' => {
-      'img' => 'github.png',
-      'url' => 'https://github.com/XXX',
-      'placeholder' => 'username',
-      'label' => 'GitHub',
-      'position' => 5,
-      'icon' => 'github',
-      're' => /^[A-Za-z0-9_.-]+$/
+    {
+      tag: 'github',
+      # img: 'github.png',
+      url: 'https://github.com/XXX',
+      placeholder: 'username',
+      label: 'GitHub',
+      position: 5,
+      icon: 'github',
+      re: /^[A-Za-z0-9_.-]+$/
     },
-    'stack_overflow' => {
-      'img' => 'stackoverflow.png',
-      'url' => 'https://stackoverflow.com/users/XXX',
-      'placeholder' => '12345678',
-      'label' => 'Stack Overflow',
-      'position' => 6,
-      'icon' => 'stack-overflow',
-      're' => /^[0-9]+$/
+    {
+      tag: 'stack_overflow',
+      # img: 'stack-overflow.svg',
+      url: 'https://stackoverflow.com/users/XXX',
+      placeholder: '12345678',
+      label: 'Stack Overflow',
+      position: 6,
+      icon: 'icon-stackoverflow',
+      re: /^[0-9]+$/
     },
     # TODO: the mastodon address needs to be something like @instance@username
     #       and the URL have to be recomputed accordingly.
     #       Alternatively, we can limit to epfl instance social.epfl.ch
-    'mastodon' => {
-      'img' => 'mastodon.png',
-      'url' => 'https://mastodon.social/@XXX',
-      'placeholder' => 'username',
-      'label' => 'Mastodon',
-      'position' => 7,
-      'icon' => 'mastodon',
-      're' => /^[A-Za-z0-9_]+$/
+    {
+      tag: 'mastodon',
+      # img: 'mastodon.png',
+      url: 'https://social.epfl.ch/@XXX',
+      placeholder: 'username',
+      label: 'Mastodon',
+      position: 7,
+      icon: 'icon-mastodon',
+      re: /^[A-Za-z0-9_]+$/
     },
-    'facebook' => {
-      'img' => 'facebook.png',
-      'url' => 'https://www.facebook.com/XXX',
-      'placeholder' => 'john.doe',
-      'label' => 'Facebook',
-      'position' => 8,
-      'icon' => 'facebook',
-      're' => /^[A-Za-z0-9.]+$/
+    {
+      tag: 'facebook',
+      # img: 'facebook.png',
+      url: 'https://www.facebook.com/XXX',
+      placeholder: 'john.doe',
+      label: 'Facebook',
+      position: 8,
+      icon: 'icon-facebook',
+      re: /^[A-Za-z0-9.]+$/
     },
-    'twitter' => {
-      'img' => 'twitter.png',
-      'url' => 'https://twitter.com/XXX',
-      'placeholder' => 'username',
-      'label' => 'Twitter',
-      'position' => 9,
-      'icon' => 'twitter',
-      're' => /^[A-Za-z0-9_]+$/
+    {
+      tag: 'instagram',
+      # img: 'instagram.png',
+      url: 'https://www.instagram.com/XXX',
+      placeholder: '@username',
+      label: 'Instagram',
+      position: 9,
+      icon: 'instagram',
+      re: /^[A-Za-z0-9._]+$/
     },
-    'instagram' => {
-      'img' => 'instagram.png',
-      'url' => 'https://www.instagram.com/XXX',
-      'placeholder' => '@username',
-      'label' => 'Instagram',
-      'position' => 10,
-      'icon' => 'instagram',
-      're' => /^[A-Za-z0-9._]+$/
+    # {
+    #   tag: # 'twitter',
+    #   # img: 'twitter.png',
+    #   url: 'https://twitter.com/XXX',
+    #   placeholder: 'username',
+    #   label: 'Twitter',
+    #   position: 10,
+    #   icon: 'twitter',
+    #   re: /^[A-Za-z0-9_]+$/
+    # },
+    {
+      tag: 'muskidiocy',
+      url: 'https://x.com/XXX',
+      placeholder: 'username',
+      label: 'X (Twitter)',
+      position: 99,
+      icon: 'icon-x',
+      re: /^[A-Za-z0-9_]+$/
     }
-  }.freeze
+  ].freeze
+
+  RESEARCH_IDS = RESEARCH_IDS_LIST.index_by { |v| v[:tag] }.freeze
+  TAG_SELECT_OPTIONS = RESEARCH_IDS_LIST.map { |v| [v[:label], v[:tag]] }
 
   belongs_to :profile, class_name: "Profile", inverse_of: :socials
 
@@ -133,28 +156,33 @@ class Social < ApplicationRecord
   def url
     @url ||= begin
       @s = RESEARCH_IDS[tag]
-      @s['url'].sub('XXX', value)
+      @s[:url].sub('XXX', value)
     end
   end
 
   def icon_class
     @s ||= RESEARCH_IDS[tag]
-    @s['icon'].nil? ? '' : "social-icon-#{@s['icon']}"
+    @s[:icon].nil? ? '' : "social-icon-#{@s['icon']}"
   end
 
   def icon
     @s ||= RESEARCH_IDS[tag]
-    @s['icon']
+    @s[:icon]
+  end
+
+  def customicon
+    @s ||= RESEARCH_IDS[tag]
+    @s[:customicon]
   end
 
   def image
     @s ||= RESEARCH_IDS[tag]
-    "social/#{@s['img']}"
+    @s.key?(:img) ? "social/#{@s[:img]}" : nil
   end
 
   def label
     @s ||= RESEARCH_IDS[tag]
-    @s['label']
+    @s[:label]
   end
 
   def url_prefix
@@ -163,7 +191,7 @@ class Social < ApplicationRecord
 
   def default_position
     @s ||= RESEARCH_IDS[tag]
-    @s['position']
+    @s[:position]
   end
 
   private
@@ -178,7 +206,7 @@ class Social < ApplicationRecord
       return false
     end
 
-    re = RESEARCH_IDS[tag]['re']
+    re = RESEARCH_IDS[tag][:re]
     return if re.match?(value)
 
     errors.add(:value, I18n.t('activerecord.errors.models.social.attributes.value.incorrect_format'))
@@ -188,12 +216,5 @@ class Social < ApplicationRecord
   # TODO: fire a request to the url and check if it actually exist
   def url_actually_exists
     true
-  end
-
-  def link_type_tag(url)
-    RESEARCH_IDS.each_value do |value|
-      return content_tag(:span, value['label'], class: 'tag tag-sm tag-primary') if url.match(value['re'])
-    end
-    content_tag(:span, 'Unknown', class: 'tag tag-sm tag-secondary')
   end
 end
