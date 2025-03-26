6ddb19d1774edc3a660cd8679e229dfe395e226d
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-03-26 19:05:40 +0100

    [feat] two step social creation
     - this enables automatic socials (= links that can be auto-filled by the server)
     - I have tried several options for how to select the type of social to create but in the end a list of simple buttons is the easiest and fastest (in any case the only one with just a single click)
    This one took a lot of time but helped to get rid of a long standing issue with turbo.

--- app/javascript/controllers/socials_controller.js
+++ app/javascript/controllers/socials_controller.js
@@ -1,52 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["urlPrefix", "valueField", "tagSelect"];
+  static targets = ["urlPreview", "valueField", "checkButton"];
+  static values = { pattern: String }
 
-  connect() {
-    this.updateUrlPrefix();
+  url() {
+    const value = this.valueFieldTarget.value.trim();
+    return value ? this.patternValue.replace("XXX", value) : null
   }
 
-  updateUrlPrefix() {
-    const selectedTagOption = this.tagSelectTarget.selectedOptions[0];
-    const baseUrl = selectedTagOption.getAttribute('data-url-prefix');
-    const id = this.valueFieldTarget.value.trim();
-
-    if (baseUrl) {
-      this.urlPrefixTarget.textContent = id ? `${baseUrl}${id}` : baseUrl;
-      this.valueFieldTarget.placeholder = selectedTagOption.getAttribute('data-placeholder');
+  onValueChange() {
+    const url = this.url();
+    this.urlPreviewTarget.textContent = url;
+    if(url) {
+      this.checkButtonTarget.disabled = false;
+    } else {
+      this.checkButtonTarget.disabled = true;
     }
-  }
-
-  sanitizeValue() {
-    const inputValue = this.valueFieldTarget.value.trim();
-
-    for (const option of this.tagSelectTarget.options) {
-      const url = option.getAttribute('data-url-prefix');
-      if (inputValue.startsWith(url)) {
-        this.valueFieldTarget.value = inputValue.replace(url, '');
-
-        this.urlPrefixTarget.textContent = `${url}${this.valueFieldTarget.value}`;
-
-        // WARNING: This works thanks to elements.js which makes select easier
-        // to work with. Normally one should remove/add the "selected" property
-        // on the options tags.
-        this.tagSelectTarget.value = option.value;
-
-        this.valueFieldTarget.placeholder = option.getAttribute('data-placeholder');
-        break;
-      }
-    }
-  }
-
-  tagChanged(event) {
-    this.updateUrlPrefix();
   }
 
   checkLink() {
-    const baseUrl = this.urlPrefixTarget.textContent;
-    if (baseUrl) {
-      window.open(baseUrl, '_blank');
+    const url = this.url();
+    if(url) {
+      window.open(this.url(), '_blank');
     }
   }
 }
