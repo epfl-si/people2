293f8d9ca3276cea05684f0430bc8da0537fc8cc
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-03-26 14:33:39 +0100

    [fix] goodbye animations
    Removing this hack because it introduces a lot of bugs in turbo leading
    to ineffective turbo_stream.update calls

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,34 +3,3 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
-
-// Animate turbo frame replacements (https://edforshaw.co.uk/hotwire-turbo-stream-animations)
-// TODO: I think this induces some strange behaviour to the turbo-replacement
-// thing preventing edit of a newly created record.
-document.addEventListener("turbo:before-stream-render", function(event) {
-  let elementToAdd = null;
-
-  // Add "turbo-replace-enter" class to an element we are about to add to the page
-  if (event.target.firstElementChild instanceof HTMLTemplateElement) {
-    elementToAdd = event.target.templateElement.content.firstElementChild
-    if (elementToAdd) {
-      elementToAdd.classList.add("turbo-replace-enter")
-    }
-  }
-
-  // Add "turbo-replace-enter" class to the element we are about to remove from the page
-  const elementToRemove = document.getElementById(event.target.target)
-  if (elementToRemove) {
-    event.preventDefault()
-    elementToRemove.classList.add("turbo-replace-exit")
-    // Wait for its animation to end before removing the element
-    elementToRemove.addEventListener("animationend", function() {
-      event.target.performAction()
-      if (elementToAdd) {
-        elementToAdd.classList.remove("turbo-replace-enter")
-      }
-    })
-  }
-})
-
-
