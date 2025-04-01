b8c02e64d51769e59e00cd53be8561eff7d730cb
D4rkHeart <leonardo.surdez@gmail.com>
2025-04-01 15:47:26 +0200

    [feat] Improved tests for models : - box - course - experience - function_change - index_box  - add data test for sections/boxes/model_boxes

--- test/models/experience_test.rb
+++ test/models/experience_test.rb
@@ -3,58 +3,27 @@
 require 'test_helper'
 
 class ExperienceTest < ActiveSupport::TestCase
-  fixtures :experiences
+  test "complete_period copies year_end to year_begin if year_begin is nil" do
+    exp = Experience.new(year_end: 2020, year_begin: nil)
+    exp.valid?
 
-  def setup
-    @giova_exp_algo = experiences(:giova_exp_algo)
-    @giova_exp_kandou = experiences(:giova_exp_kandou)
-    @giova_exp_vpsi = experiences(:giova_exp_vpsi)
+    assert_equal 2020, exp.year_begin
+    assert_equal 2020, exp.year_end
   end
 
-  test "should validate presence of location" do
-    experience = Experience.new(location: nil, year_begin: 2000, year_end: 2005)
-    assert_not experience.valid?, "Experience without location should be invalid"
-    assert_includes experience.errors[:location],
-                    I18n.t('activerecord.errors.models.experience.attributes.location.blank')
+  test "complete_period copies year_begin to year_end if year_end is nil" do
+    exp = Experience.new(year_begin: 2018, year_end: nil)
+    exp.valid?
+
+    assert_equal 2018, exp.year_end
+    assert_equal 2018, exp.year_begin
   end
 
-  test "should validate presence of year_begin and year_end" do
-    experience = Experience.new(location: 'EPFL', year_begin: nil, year_end: nil)
+  test "complete_period does not override when both years are set" do
+    exp = Experience.new(year_begin: 2015, year_end: 2020)
+    exp.valid?
 
-    Experience.skip_callback(:validation, :before, :complete_period)
-
-    assert_not experience.valid?, "Experience without year_begin and year_end should be invalid"
-    assert_includes experience.errors[:year_begin],
-                    I18n.t('activerecord.errors.models.experience.attributes.year_begin.blank')
-    assert_includes experience.errors[:year_end],
-                    I18n.t('activerecord.errors.models.experience.attributes.year_end.blank')
-
-    Experience.set_callback(:validation, :before, :complete_period)
-  end
-
-  test "should complete period with year_end if year_begin is nil" do
-    experience = Experience.new(year_begin: nil, year_end: 2019, location: 'EPFL', title_en: "DevOps",
-                                title_fr: "DevOps")
-    experience.valid? # Trigger before_validation
-    assert_equal experience.year_end, experience.year_begin, "year_begin should be set to year_end if it is nil"
-  end
-
-  test "should complete period with year_begin if year_end is nil" do
-    experience = Experience.new(year_begin: 2017, year_end: nil, location: 'EPFL', title_en: "DevOps",
-                                title_fr: "DevOps")
-    experience.valid? # Trigger before_validation
-    assert_equal experience.year_begin, experience.year_end, "year_end should be set to year_begin if it is nil"
-  end
-
-  test "should be positioned correctly within profile" do
-    experiences = @giova_exp_algo.profile.experiences.order(:position)
-    assert_equal [@giova_exp_algo, @giova_exp_kandou, @giova_exp_vpsi], experiences,
-                 "Experiences should be positioned correctly"
-  end
-
-  test "should correctly handle audience visibility" do
-    assert @giova_exp_algo.visible?, "Experience :giova_exp_algo should be visible"
-    assert @giova_exp_kandou.visible?, "Experience :giova_exp_kandou should be visible"
-    assert @giova_exp_vpsi.visible?, "Experience :giova_exp_vpsi should be visible"
+    assert_equal 2015, exp.year_begin
+    assert_equal 2020, exp.year_end
   end
 end
