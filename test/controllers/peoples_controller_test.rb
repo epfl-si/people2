be0a70b7dfdf0d4c9246c338e738b57b6692732a
D4rkHeart <leonardo.surdez@gmail.com>
2025-04-10 11:36:24 +0200

    [chore]: Comment out tests not yet updated and indicate empty tests - Added clear comments to mark tests that are not yet written or need review. - Signaled explicitly when a test file is empty or pending for a valid reason. - Helps to track missing or pending tests for future implementation.

--- test/controllers/peoples_controller_test.rb
+++ test/controllers/peoples_controller_test.rb
@@ -4,67 +4,69 @@ require "test_helper"
 require "webmock/minitest"
 
 class PeopleControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @person_data = {
-      'id' => '110635',
-      'firstname' => 'Giovanni',
-      'lastname' => 'Rossi',
-      'firstnameusual' => 'Gio',
-      'lastnameusual' => 'Rossi',
-      'position' => { 'title' => 'Professor' },
-      'gender' => 'male', # Added gender
-      'phones' => [
-        { 'unit_id' => '101', 'number' => '12345', 'visible' => true },
-        { 'unit_id' => '101', 'number' => '67890', 'visible' => false }
-      ],
-      'addresses' => [
-        { 'unitid' => '101', 'part1' => 'Office', 'part2' => 'EPFL', 'fromdefault' => '1' }
-      ],
-      'account' => { 'sciper' => '110635' },
-      'automap' => { 'field1' => 'value1' },
-      'camipro' => { 'card_number' => '1234' },
-      'email' => 'giovanni.rossi@epfl.ch'
-    }
+  # This test has not yet been written or needs to be reviewed.
+  # Please implement or revise it to ensure full test coverage.
+  # setup do
+  #   @person_data = {
+  #     'id' => '110635',
+  #     'firstname' => 'Giovanni',
+  #     'lastname' => 'Rossi',
+  #     'firstnameusual' => 'Gio',
+  #     'lastnameusual' => 'Rossi',
+  #     'position' => { 'title' => 'Professor' },
+  #     'gender' => 'male', # Added gender
+  #     'phones' => [
+  #       { 'unit_id' => '101', 'number' => '12345', 'visible' => true },
+  #       { 'unit_id' => '101', 'number' => '67890', 'visible' => false }
+  #     ],
+  #     'addresses' => [
+  #       { 'unitid' => '101', 'part1' => 'Office', 'part2' => 'EPFL', 'fromdefault' => '1' }
+  #     ],
+  #     'account' => { 'sciper' => '110635' },
+  #     'automap' => { 'field1' => 'value1' },
+  #     'camipro' => { 'card_number' => '1234' },
+  #     'email' => 'giovanni.rossi@epfl.ch'
+  #   }
 
-    @accreditations_data = {
-      'accreds' => [
-        {
-          'id' => '1',
-          'label' => 'Professeur',
-          'visible' => true,
-          'unit' => { 'id' => 101, 'name' => 'EPFL', 'labelfr' => 'Lab' },
-          'status' => { 'id' => 1, 'labelfr' => 'Actif' },
-          'position' => { 'labelen' => 'Professor', 'labelfr' => 'Professeur' },
-          'role' => 'Professor' # Ensure role is always present
-        }
-      ]
-    }
+  #   @accreditations_data = {
+  #     'accreds' => [
+  #       {
+  #         'id' => '1',
+  #         'label' => 'Professeur',
+  #         'visible' => true,
+  #         'unit' => { 'id' => 101, 'name' => 'EPFL', 'labelfr' => 'Lab' },
+  #         'status' => { 'id' => 1, 'labelfr' => 'Actif' },
+  #         'position' => { 'labelen' => 'Professor', 'labelfr' => 'Professeur' },
+  #         'role' => 'Professor' # Ensure role is always present
+  #       }
+  #     ]
+  #   }
 
-    # Stub for persons API
-    stub_request(:get, "https://api.epfl.ch/v1/persons/110635")
-      .to_return(status: 200, body: @person_data.to_json,
-                 headers: { 'Content-Type' => 'application/json' })
+  #   # Stub for persons API
+  #   stub_request(:get, "https://api.epfl.ch/v1/persons/110635")
+  #     .to_return(status: 200, body: @person_data.to_json,
+  #                headers: { 'Content-Type' => 'application/json' })
 
-    # Stub for accreditations API
-    stub_request(:get, "https://api.epfl.ch/v1/accreds?persid=110635")
-      .to_return(status: 200, body: @accreditations_data.to_json,
-                 headers: { 'Content-Type' => 'application/json' })
+  #   # Stub for accreditations API
+  #   stub_request(:get, "https://api.epfl.ch/v1/accreds?persid=110635")
+  #     .to_return(status: 200, body: @accreditations_data.to_json,
+  #                headers: { 'Content-Type' => 'application/json' })
 
-    # Stub for authorization API
-    stub_request(:get, "https://api.epfl.ch/v1/authorizations?authid=botweb&persid=110635&status=active&type=property")
-      .to_return(status: 200, body: { "authorizations" => [{ "unit_id" => 101,
-                                                             "authorized" => true }] }.to_json,
-                 headers: { 'Content-Type' => 'application/json' })
+  #   # Stub for authorization API
+  #   stub_request(:get, "https://api.epfl.ch/v1/authorizations?authid=botweb&persid=110635&status=active&type=property")
+  #     .to_return(status: 200, body: { "authorizations" => [{ "unit_id" => 101,
+  #                                                            "authorized" => true }] }.to_json,
+  #                headers: { 'Content-Type' => 'application/json' })
 
-    # Stub for units API
-    stub_request(:get, "https://api.epfl.ch/v1/units/101")
-      .to_return(status: 200, body: { 'name' => 'EPFL Unit',
-                                      'labelfr' => 'Laboratoire' }.to_json,
-                 headers: { 'Content-Type' => 'application/json' })
+  #   # Stub for units API
+  #   stub_request(:get, "https://api.epfl.ch/v1/units/101")
+  #     .to_return(status: 200, body: { 'name' => 'EPFL Unit',
+  #                                     'labelfr' => 'Laboratoire' }.to_json,
+  #                headers: { 'Content-Type' => 'application/json' })
 
-    @visible_accreditation = OpenStruct.new(visible?: true, label: 'Professeur', role: 'Professor',
-                                            prefs: OpenStruct.new(visible?: true))
-  end
+  #   @visible_accreditation = OpenStruct.new(visible?: true, label: 'Professeur', role: 'Professor',
+  #                                           prefs: OpenStruct.new(visible?: true))
+  # end
 
   # test "should show person profile with correct title if profile is visible" do
   #   Accreditation.stub(:for_sciper, [@visible_accreditation]) do
