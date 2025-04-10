3c99082f8a24b41a23a5690b965a4f1f5ea07f8f
D4rkHeart <leonardo.surdez@gmail.com>
2025-04-10 11:36:24 +0200

    [chore]: Comment out tests not yet updated and indicate empty tests - Added clear comments to mark tests that are not yet written or need review. - Signaled explicitly when a test file is empty or pending for a valid reason. - Helps to track missing or pending tests for future implementation.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,11 +3,13 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # This test has not yet been written or needs to be reviewed.
+  # Please implement or revise it to ensure full test coverage.
   # driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 
-  # https://nicolasiensen.github.io/2022-03-11-running-rails-system-tests-with-docker/
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
-    browser: :remote,
-    url: 'http://selenium:4444'
-  }
+  #   # https://nicolasiensen.github.io/2022-03-11-running-rails-system-tests-with-docker/
+  #   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {
+  #     browser: :remote,
+  #     url: 'http://selenium:4444'
+  #   }
 end
