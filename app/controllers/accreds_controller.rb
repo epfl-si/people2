bf58b1ee1f2c7369da99f5d46c2bb784fbc98c5e
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-04-29 19:06:43 +0200

    [fix] sorting of accreds ineffective

--- app/controllers/accreds_controller.rb
+++ app/controllers/accreds_controller.rb
@@ -6,7 +6,7 @@ class AccredsController < ApplicationController
 
   # GET /profile/profile_id/accreds or /profile/profile_id/accreds.json
   def index
-    @accreds = Accred.for_profile!(@profile).sort
+    @accreds = Accred.for_profile!(@profile).sort { |a, b| a.position <=> b.position }
   end
 
   # GET /accreds/1 or /accreds/1.json
