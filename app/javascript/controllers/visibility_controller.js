d5676b7363bb990718eeca1d0d3bcb840cee5592
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-05-06 15:30:55 +0200

    [wip] working with cliccable rows + group by files

--- app/javascript/controllers/visibility_controller.js
+++ app/javascript/controllers/visibility_controller.js
@@ -1,10 +1,10 @@
 // app/javascript/controllers/visibility_controller.js
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import { put } from "@rails/request.js";
 
 export default class extends Controller {
   static values = { model: String, field: String };
-  static targets = [ "form", "radio", "label"];
+  static targets = ["form", "radio", "label"];
 
   connect() {
     this.oldRadio = this.radioTargets.find((x) => x.checked);
@@ -21,15 +21,15 @@ export default class extends Controller {
     const newRadio = this.radioTargets.find((x) => x.checked);
 
     const value = newRadio.value;
-    const label = newRadio.getAttribute('data-label');
+    const label = newRadio.getAttribute("data-label");
     // JS shit!
     const body = {};
     body[this.modelValue] = {};
     body[this.modelValue][this.fieldValue] = value;
-    put(this.url, {body: body}).then((response) => {
+    put(this.url, { body: body }).then((response) => {
       if (response.ok) {
         this.oldRadio = newRadio;
-        this.labelTarget.textContent=label;
+        this.labelTarget.textContent = label;
       } else {
         // TODO: add flash message on error
         // prevent onChange to fire again
