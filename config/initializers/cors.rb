509c40791bbbf44d4d86f22b920170b520590933
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-05-19 14:19:12 +0200

    [chore] bump elements version to 8.0.0

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # https://github.com/cyu/rack-cors
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'https://web2018.epfl.ch/'
-    resource '*', headers: :any, methods: %i[get]
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'origin1','origin2'
+#     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+#   end
+# end
