54f1e33ce9574b1eec2d921d8344bc7f9077050c
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-05-22 15:24:32 +0200

    [refact] name change feature to make it more RoRish

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -101,7 +101,7 @@ class Profile < ApplicationRecord
     where(sciper: sciper).first
   end
 
-  delegate :gender, to: :person
+  delegate :doctoral?, :gender, :name, :staff?, :student?, to: :person
 
   # I have tried to put all these flags into a single
   def translations
