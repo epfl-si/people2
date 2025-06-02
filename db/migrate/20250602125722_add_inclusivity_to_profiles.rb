66417d773435349c04656073ce41d7082e3767a6
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-06-02 22:28:20 +0200

    [fix] layout and effectiveness for inclusivity

--- db/migrate/20250602125722_add_inclusivity_to_profiles.rb
+++ db/migrate/20250602125722_add_inclusivity_to_profiles.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddInclusivityToProfiles < ActiveRecord::Migration[8.1]
+class AddInclusivityToProfiles < ActiveRecord::Migration[8.0]
   def change
     add_column :profiles, :inclusivity, :boolean, default: false, null: false
   end
