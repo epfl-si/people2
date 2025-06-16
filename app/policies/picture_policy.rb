9e10bdac40a6d1e9847d640af9b3f43f35d4ef51
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-06-16 18:42:17 +0200

    [fix] acl special for photos

--- app/policies/picture_policy.rb
+++ app/policies/picture_policy.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
-class PicturePolicy < ProfilePolicy
+class PicturePolicy < ApplicationPolicy
+  # «Pour des raisons légales, il est impossible de déléguer la gestion de sa photo de profil à autrui.»
+  def update?
+    owner?
+  end
 end
