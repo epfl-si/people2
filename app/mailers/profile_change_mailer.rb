44072b16df8cab45a52c5a8f0b774492ed1a1cf6
D4rkHeart <leonardo.surdez@gmail.com>
2025-06-27 11:49:08 +0200

    [refact] rename FunctionChangeMailer to ProfileChangeMailer and add name change mailer - replaces old function mailer with generic profile mailer - adds name_change_request method for accreditor notification

---
+++ app/mailers/profile_change_mailer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class ProfileChangeMailer < ApplicationMailer
+  def function_change_request(profile, reason)
+    @profile = profile
+    @reason  = reason
+    recipients = profile.accreditors.map(&:email)
+    mail(to: recipients, subject: "Demande de changement de fonction pour #{profile.name}")
+  end
+
+  def name_change_request
+    @request = params[:name_change_request]
+    sciper = params[:accreditor_sciper]
+
+    @accreditor = @request.available_accreditors[sciper]
+    person = @request.profile.person
+    subject_text = "Demande de changement de nom usuel – #{person.name.display} (#{person.sciper})"
+
+    mail(to: @accreditor.email, subject: subject_text)
+  end
+end
