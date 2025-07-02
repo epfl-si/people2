4686c9c6f95fbc8aba89fc207569d4eac8d5e5ea
D4rkHeart <leonardo.surdez@gmail.com>
2025-07-02 13:53:30 +0200

    [feat] remove accreditor logic

--- app/controllers/name_change_requests_controller.rb
+++ app/controllers/name_change_requests_controller.rb
@@ -2,24 +2,19 @@
 
 class NameChangeRequestsController < ApplicationController
   before_action :load_profile
-  before_action :accreditors_from_first_accreditation
 
   def new
-    @name_change_request = NameChangeRequest.new
-    @accreditors = accreditors_from_first_accreditation
+    @name_change_request = NameChangeRequest.for(@profile) || NameChangeRequest.new
   end
 
   def create
-    @name_change_request = NameChangeRequest.new(name_change_request_params)
-    @name_change_request.profile = @profile
+    @name_change_request = NameChangeRequest.for(@profile) || NameChangeRequest.new
+    @name_change_request.assign_attributes(name_change_request_params)
 
     if @name_change_request.save
-      @name_change_request.selected_accreditors.each_key do |sciper|
-        ProfileChangeMailer.with(
-          name_change_request: @name_change_request,
-          accreditor_sciper: sciper
-        ).name_change_request.deliver_later
-      end
+      ProfileChangeMailer.with(name_change_request: @name_change_request)
+                         .name_change_request
+                         .deliver_later
 
       respond_to do |format|
         format.turbo_stream
@@ -29,7 +24,6 @@ class NameChangeRequestsController < ApplicationController
         end
       end
     else
-      @accreditors = accreditors_from_first_accreditation
       render :new, status: :unprocessable_entity
     end
   end
@@ -40,13 +34,8 @@ class NameChangeRequestsController < ApplicationController
     params.require(:name_change_request).permit(
       :new_first,
       :new_last,
-      :reason,
-      accreditor_scipers: []
+      :reason
     )
-  end
-
-  def accreditors_from_first_accreditation
-    @profile&.person&.accreditations&.first&.accreditors || []
   end
 
   def load_profile
