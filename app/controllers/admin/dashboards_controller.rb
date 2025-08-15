b0e935940bae2322a5511af11a82ae7519f6d5ec
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-08-15 12:12:33 +0200

    [feat] nicer admin dashboard for adoptions
    actually having fun with css grid

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -2,21 +2,34 @@
 
 module Admin
   class DashboardsController < BaseController
+    # This way, one day we can make this user-configurable
+    PANELS = [
+      { name: "adoplatest", enabled: true, gridcol: ["auto", 3], gridrow: ["auto", 3] },
+      { name: "adopcount", enabled: true, gridcol: ["auto", 1], gridrow: ["auto", 1] },
+      { name: "scipercount", enabled: true, period: 1_800, gridcol: ["auto", 1], gridrow: ["auto", 1] }
+    ].map { |h| [h[:name], OpenStruct.new({ period: 60 }.merge(h))] }.to_h.freeze
+
     def index
-      @panels = %w[adopcount adoplatest]
+      @panels = PANELS.values.select(&:enabled)
     end
 
     def show
-      @panel = params[:id]
-      raise ActionController::RoutingError unless respond_to?(@panel)
+      @panel = PANELS[params[:id]]
+      raise ActionController::RoutingError if @panel.blank?
 
-      send(@panel)
+      @interval = 60 # seconds
+
+      send(@panel.name)
     end
 
     def adopcount
       @ado_tot = Adoption.count
       @ado_acc = Adoption.accepted.count
       @ado_orp = @ado_tot - @ado_acc
+    end
+
+    def scipercount
+      @interval = 1_800 # this is essentially constant => no need to reload often
       @peo_tot = Work::Sciper.count
       @peo_nop = Work::Sciper.noprofile.count
       @peo_yes = Work::Sciper.migrated.count
--- app/javascript/controllers/autoreload_controller.js
+++ app/javascript/controllers/autoreload_controller.js
@@ -7,17 +7,20 @@ export default class extends Controller {
   connect() {
     console.log("autoreload_controller connected");
     console.log(this.element);
-    this.launch_reload();
+    // first reload is fired at randomized time (v% of/around intervalValue)
+    const v = 20
+    const i = this.intervalValue + Math.floor((Math.random() - 0.5) * this.intervalValue * v / 100);
+    this.launch_reload(i);
   }
 
-  launch_reload() {
+  launch_reload(i) {
     var o = this;
     setTimeout(
       function() {
         o.element.reload();
-        o.launch_reload();
+        o.launch_reload(o.intervalValue);
       },
-      o.intervalValue
+      i
     )
   }
 }
