4e7cea25ba711950ceda2e08993e2fa4891a1018
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-08-19 18:22:58 +0200

    [chore] auth the superuser to edit the picture
    Might not be fully compliant with the lex but enable more efficient support to users

--- app/policies/picture_policy.rb
+++ app/policies/picture_policy.rb
@@ -2,7 +2,8 @@
 
 class PicturePolicy < ApplicationPolicy
   # «Pour des raisons légales, il est impossible de déléguer la gestion de sa photo de profil à autrui.»
+  # I still keep supersuer for debugging / support purpose
   def update?
-    owner?
+    owner_or_su?
   end
 end
