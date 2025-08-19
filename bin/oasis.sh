556110e3d3d1a7b0b1eceb749edd3aea94130765
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-08-19 18:20:47 +0200

    [doc] helper script for fetching data from Oasis (new ISA)

---
+++ bin/oasis.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# set -x
+# Wrapper script for http queries to the new Oasis server
+# that is going to replace ISA
+
+. ./.env
+. ${KBPATH:-/keybase/team/epfl_people.prod}/${SECRETS:-secrets_prod.sh}
+BASE=${OASIS_BASEURL:-https://oasis-t.epfl.ch:8484}
+
+apiget() {
+  url="$1"
+  curl    -X GET -H "Authorization: Bearer ${OASIS_BEARER}" -H "accept: application/json" $url 2>/dev/null
+  # curl -I -X GET -H "Authorization: Bearer ${OASIS_BEARER}" -H 'accept: application/json' $url
+}
+
+jqr=""
+data=""
+while [ $# -gt 0 ] ; do
+case $1 in
+-r)
+  jqr="$2"
+  shift 2
+  ;;
+-v)
+  verbose=1
+  echo "Using base $BASE"
+  shift 1
+  ;;
+*)
+  url="$1"
+  shift 1
+  ;;
+esac
+done
+
+if [[ "$url" != ${BASE}* ]] ; then
+  url="${BASE}/$url"
+fi
+
+if [ -n "$jqr" ] ; then
+  apiget "$url" "$data"  | jq -r "$jqr"
+else
+  apiget "$url" "$data"
+fi
