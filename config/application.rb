f7e1227a162b1cfdc34d01c9a8090199aed3625b
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2025-09-16 10:00:25 +0200

    [refeature] As per (oral) review, bring back `SHOW_ERROR_PAGES`

--- config/application.rb
+++ config/application.rb
@@ -86,7 +86,7 @@ module People
     # which will probably become config maps
 
     # ENV vars that are read by files in environments/*.rb
-    # development: REDIS_CACHE
+    # development: REDIS_CACHE, SHOW_ERROR_PAGES
     # production: RAILS_LOG_LEVEL
     # db/seed.rb: DEV_SEEDS_PATH, SEEDS_PATH
 
