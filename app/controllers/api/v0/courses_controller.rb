259fc4e63cbce0485b145af72b9ee2af39bc23e5
Nicolas Borboën <ponsfrilus@gmail.com>
2025-09-24 16:05:34 +0200

    [feature] Mock data
    (cherry picked from commit 5b3b6f428b298adde47bc7635a7ffc5e5e218f10)

--- app/controllers/api/v0/courses_controller.rb
+++ app/controllers/api/v0/courses_controller.rb
@@ -7,6 +7,23 @@ module API
         # if query string got format=html
         raise NotImplementedError if params["format"] != "html"
 
+        # Mock data
+        @courses = [OpenStruct.new(
+          title: "Drawing structures",
+          teachers: ["Fernandez-Ordoñez Hernandez David Carlos", "Baur Raffael", "Guaita Patricia"],
+          slug: "CIVIL-126",
+          isa_url: "http://isa.epfl.ch/imoniteur_ISAP/!itffichecours.htm?ww_i_matiere=3928308740&ww_x_anneeacad=2840683608&ww_i_section=942623&ww_i_niveau=6683111&ww_c_langue=fr",
+          semester: "Génie civil-Bachelor semestre 1",
+          curricular_year: "'2025-2026"
+        ),
+        OpenStruct.new(
+          title: "Engineering a sustainable built environment",
+          teachers: ["Sonta Andrew James"],
+          slug: "CIVIL-126",
+          isa_url: "http://isa.epfl.ch/imoniteur_ISAP/!itffichecours.htm?ww_i_matiere=3928308740&ww_x_anneeacad=2840683608&ww_i_section=942623&ww_i_niveau=6683111&ww_c_langue=fr",
+          semester: "Génie civil-Bachelor semestre 1",
+          curricular_year: "'2025-2026"
+        )]
         render 'api/v0/courses/index', layout: false
       end
     end
