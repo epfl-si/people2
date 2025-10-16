797085dc4b9bf4b40353044d901f0d30860d4ffa
Giovanni Cangiani <giovanni.cangiani@gmail.com>
2025-10-16 11:28:10 +0200

    [fix] missing url in imported aar awards

--- app/jobs/legacy_awards_reimport_job.rb
+++ app/jobs/legacy_awards_reimport_job.rb
@@ -22,21 +22,44 @@ class LegacyAwardsReimportJob < ApplicationJob
           next
         end
         errs = a.errors.map { |err| "#{err.attribute}: #{err.type}" }.join(", ")
-        invalids << la
         Rails.logger.error "Legacy::Award ERR #{s}/#{la.ordre}: #{errs}"
       end
     end
-    # # Rails.logger.error "Legacy::Award Invalids: #{invalids.to_json}"
-    # File.open('tmp/bad_awards.csv', 'w+') do |f|
-    #   headers = ["id", "sciper", "name", "year", "category", "origin", "title", "grantedby", "url"]
-    #   f.puts CSV.generate_line headers
-    #   invalids.each do |ia|
-    #     per=Person.find(ia.sciper)
-    #     f.puts CSV.generate_line([
-    #       ia.id, ia.sciper, per.name.display, ia.year, ia.category, ia.origin, ia.title, ia.grantedby, ia.url
-    #     ])
-    #   end
-    # end
     nil
   end
 end
+
+# # This was done to export a csv file for manual revision
+# require 'csv'
+# laa = Legacy::Award.where(sciper: Work::Sciper.migranda.pluck(:sciper))
+# # scipers of existing profiles
+# pss = Profile.pluck(:sciper)
+# # legacy and new awards scipers
+# lss = laa.map(&:sciper).uniq.intersection(pss)
+# nss = Award.joins(:profile).map(&:sciper).uniq
+# # scipers that we have to treat
+# ss = lss - nss
+# bad = []
+# ss.each do |s|
+#   pro = Profile.for_sciper(s)
+#   Legacy::Award.where(sciper: s).find_each do |la|
+#     a = la.to_award('en', profile: pro, fallback_lang: 'en')
+#     bad << la unless a.valid?
+#   end
+# end
+# puts "#{bad.count} still invalid"
+# File.open('tmp/aaa.csv', 'w+') do |f|
+#   headers = ["id", "sciper", "year", "category", "origin", "grantedby", "title", "url"]
+#   f.puts CSV.generate_line headers
+#   bad.each do |a|
+#     f.puts CSV.generate_line [a.id, a.sciper, a.year, a.category, a.origin, a.grantedby, a.title, a.url]
+#   end
+# end
+# # once fixed:
+# d=CSV.read('tmp/aaa.csv')
+# File.open("tmp/aaa.sql", 'w+') do |f|
+#   d[1..].each do |l|
+#     f.puts "update awards set category='#{l[3]}', origin='#{l[4]}' where id=#{l[0]}"
+#   end
+# end
+# # tmp/aaa.sql sent to the legacy prod DB
